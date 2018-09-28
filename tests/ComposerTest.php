7035e4d2dca98667963ad5ee65e0b4f4322c7103
shillo <sshilo@gmail.com>
2018-09-28 03:23:52 +0300

    Setup PHPUnit autoload in phpunit.xml

---
+++ tests/ComposerTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace TwfChildTheme\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ComposerTest extends TestCase
+{
+    public function testAutoloadCanLoadLibraryClasses() : void
+    {
+    }
+}
