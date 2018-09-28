1d7b8d9d53b5e966a7b1a8941201148ab993b01c
shillo <sshilo@gmail.com>
2018-09-28 04:18:18 +0300

    Completed Composer autoload test

--- tests/ComposerTest.php
+++ tests/ComposerTest.php
@@ -4,12 +4,56 @@ declare(strict_types=1);
 namespace TwfChildTheme\Tests;
 
 use PHPUnit\Framework\TestCase;
-use TwfChildTheme\Tests\Helpers\EmptyClass;
+use Symfony\Component\Filesystem\Exception\IOException;
+use TwfChildTheme\Tests\Helpers\Filesystem as FilesystemHelper;
 
+/**
+ * Tests Composer functionality.
+ *
+ * @package TwfChildTheme\Tests
+ */
 class ComposerTest extends TestCase
 {
+    const EMPTYCLASS_DEST_FILENAME = FilesystemHelper::LIBRARY_PATH.'/tmp/EmptyClass.php';
+
     public function testAutoloadCanLoadLibraryClasses() : void
     {
-        
+        $this->copyEmptyClass();
+
+        $this->assertTrue(class_exists('\TwfChildTheme\Tmp\EmptyClass'));
+
+        $this->deleteEmptyClass();
+    }
+
+    /**
+     * Deletes the EmptyClass from library directory.
+     *
+     * @throws IOException
+     */
+    protected function deleteEmptyClass()
+    {
+        $fileSystem = FilesystemHelper::getFilesystem();
+
+        try {
+            $fileSystem->remove(self::EMPTYCLASS_DEST_FILENAME);
+        } catch (IOException $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Copies the EmptyClass to the library path.
+     *
+     * @throws IOException
+     */
+    protected function copyEmptyClass()
+    {
+        $fileSystem = FilesystemHelper::getFilesystem();
+
+        try {
+            $fileSystem->copy(FilesystemHelper::EMPTYCLASS_FILENAME, self::EMPTYCLASS_DEST_FILENAME);
+        } catch (IOException $e) {
+            throw $e;
+        }
     }
 }
---
+++ tests/helpers/Filesystem.php
@@ -0,0 +1,29 @@
+<?php
+namespace TwfChildTheme\Tests\Helpers;
+
+/**
+ * Class Filesystem
+ * @package TwfChildTheme\Tests\Helpers
+ */
+class Filesystem
+{
+    const EMPTYCLASS_FILENAME = __DIR__.'/EmptyClass.inc';
+    const ROOTDIR = __DIR__.'/../..';
+    const LIBRARY_PATH = self::ROOTDIR.'/library';
+
+    private static $filesystem = null;
+
+    /**
+     * Returns a \Symfony\Component\Filesystem\Filesystem singleton.
+     *
+     * @return \Symfony\Component\Filesystem\Filesystem
+     */
+    public static function getFilesystem()
+    {
+        if (self::$filesystem === null) {
+            self::$filesystem = new \Symfony\Component\Filesystem\Filesystem();
+        }
+
+        return self::$filesystem;
+    }
+}
