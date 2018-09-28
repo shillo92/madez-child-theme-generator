ba857e2e4b02ffca9d06c484ca1095233d576866
shillo <sshilo@gmail.com>
2018-09-28 03:35:30 +0300

    Added Symfony Filesystem

--- tests/ComposerTest.php
+++ tests/ComposerTest.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 namespace TwfChildTheme\Tests;
 
 use PHPUnit\Framework\TestCase;
+use TwfChildTheme\Tests\Helpers\EmptyClass;
 
 class ComposerTest extends TestCase
 {
     public function testAutoloadCanLoadLibraryClasses() : void
     {
+        
     }
 }
