843fc49b8b4f97ab78e701dbd79109ed8e927758
Shiloh <sshilo@gmail.com>
2018-10-01 23:30:50 +0000

    Added a localized wp-tests-config.php file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,8 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
+define('WP_TESTS_CONFIG_FILE_PATH', __DIR__.'/wp-tests-config.php');
+
 if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
