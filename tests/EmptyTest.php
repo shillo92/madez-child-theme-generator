e6019d9119c5820a6debe478a6a50917923d710f
Shiloh <sshilo@gmail.com>
2018-10-11 02:12:09 +0000

    Removed composer test and added EmptyTest

---
+++ tests/EmptyTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace TwfChildTheme\Tests;
+
+/**
+ * Tests empty functionality.
+ *
+ * @package TwfChildTheme\Tests
+ */
+class EmptyTest extends \WP_UnitTestCase
+{
+}
